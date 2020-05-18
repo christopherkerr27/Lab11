4e6f6052920ad57e3afe534ac82d3fc4e3ffd66a
Christopher Kerr <christopherke@truck-hero.com>
2020-05-18 18:23:25 -0400

    lab 11

---
+++ Lab11/Movie.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab11
+{
+    public class Movie
+    {
+
+        private string Title { get; set; }
+        private string Category { get; set; }
+
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+
+
+
+
+    }
+
+
+
+}
+
---
+++ Lab11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new Dictionary<string, string>();
+            movies.Add("The Wizard of Oz", "Drama");
+            movies.Add("Toy Story", "Animated");
+            movies.Add("Casablanca", "Drama");
+            movies.Add("The Hunt", "Horror");
+            movies.Add("Despicable Me", "Animated");
+            movies.Add("Spiderman", "Animated");
+            movies.Add("Star Wars", "Scifi");
+            movies.Add("The Lodge", "Horror");
+            movies.Add("King Kong", "Scifi");
+            movies.Add("Us", "Horror");
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine("There are 10 movies in the list.");
+            Console.WriteLine("What category are yoiu interested in?");
+            string v = Console.ReadLine();
+            string category = v;
+
+
+            {
+                System.Console.WriteLine(movies);
+            }
+        }
+
+
+    }
+
+}
+
